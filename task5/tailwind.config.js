d221eb1a15465621de631a80243ab548dc1a98d2
Gayane-043 <mgayane0403.gmail.com>
2023-12-19 19:49:22 +0400

    one

---
+++ task5/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: '#FFDD2D',
+        secondary: '#FCC521',
+      },
+    },
+  },
+  plugins: [],
+};
