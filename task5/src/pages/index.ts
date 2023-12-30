f4b2fdc3a48c0fd426116a02372eb380b8442eb8
Gayane-043 <mgayane0403.gmail.com>
2023-12-30 19:49:41 +0400

    nov

--- task5/src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as MovieDetails } from './MovieDetails';
-export { default as MovieForm } from './MovieForm';
-export { default as Main } from './Main';
